e285ac92658cf5ff3c0e18e0f569afa619be4b8d
take-cantik <miki.takeru508@mail.kyutech.jp>
2020-11-28 21:55:06 +0900

    Do 11

---
+++ main11_15.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+// 11, 整数二つを引数とし、第 1 引数が第 2 引数で割り切れたら 1、 そうでない時 0 を返す関数 int divide(int x, int y).
+
+/*if文を使って、x % y == 0の結果が真なら1、偽なら0を返すような関数を作った。*/
+
+int divide(int x, int y) {
+  if (x % y == 0) {
+    return 1;
+  } else {
+    return 0;
+  }
+}
+
+int main(void) {
+  printf("No11\n");
+  printf("%i\n", divide(12, 3));
+  printf("%i\n", divide(2, 3));
+
+  printf("No12\n");
+
+  printf("No13\n");
+
+  printf("No14\n");
+
+  printf("No15\n");
+
+
+  return 0;
+}
