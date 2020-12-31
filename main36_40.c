99d771c2883040d1f6c5f8594234f675a67300e4
take-cantik <miki.takeru508@mail.kyutech.jp>
2020-12-31 22:34:01 +0900

    Create 36 to 40

---
+++ main36_40.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+// 36, 
+
+/**/
+
+// 37, 
+
+/**/
+
+// 38, 
+
+/**/
+
+// 39, 
+
+/**/
+
+// 40, 
+
+/**/
+
+int main(void) {
+  printf("No36\n");
+
+  printf("No37\n");
+
+  printf("No38\n");
+
+  printf("No39\n");
+
+  printf("No40\n");
+  
+  return 0;
+}
+
