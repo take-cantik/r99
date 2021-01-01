5e0f5aa6294d9b1f1930231c63d3688ccf388b88
take-cantik <miki.takeru508@mail.kyutech.jp>
2021-01-01 17:15:39 +0900

    Do 36

--- main36_40.c
+++ main36_40.c
@@ -1,8 +1,25 @@
 #include <stdio.h>
 
-// 36, 
+// 36, 整数一 つを引数とし、その数が完全数かどうかを判定する関数 int is_perfect(int n).
 
-/**/
+/*完全数は自身の数は含まないので、sum_of_divisors関数がnの二倍と等しくなると完全数と言える。*/
+
+int sum_of_divisors(int n) {
+  int i;
+  int sum = 0;
+
+  for (i = 1; i <= n; i++) {
+    if (n % i == 0) {
+      sum += i;
+    }
+  }
+
+  return sum;
+}
+
+int is_perfect(int n) {
+  return sum_of_divisors(n) == 2 * n;
+}
 
 // 37, 
 
@@ -22,6 +39,7 @@
 
 int main(void) {
   printf("No36\n");
+  printf("%i\n", is_perfect(496));
 
   printf("No37\n");
 
