6102cb6d8159f07b88e11a52f1d145edce836994
take-cantik <miki.takeru508@mail.kyutech.jp>
2021-01-03 19:55:09 +0900

    Create main41_45.c

---
+++ main41_45.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+// 41,
+
+/* */
+
+// 42, 
+
+/**/
+
+// 43, 
+
+/**/
+
+// 44, 
+
+/**/
+
+// 45, 
+
+/**/
+
+int main(void) {
+  printf("No41\n");
+
+  printf("No42\n");
+
+  printf("No43\n");
+
+  printf("No44\n");
+
+  printf("No45\n");
+  
+  return 0;
+}
+
