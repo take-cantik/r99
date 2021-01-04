1dd90878e95278ef7e09d1e23decd5b1723ba6ce
take-cantik <miki.takeru508@mail.kyutech.jp>
2021-01-04 20:34:32 +0900

    Do 41

--- main41_45.c
+++ main41_45.c
@@ -1,8 +1,24 @@
 #include <stdio.h>
 
-// 41,
+// 41, 平年の1 月 1 日から同年 mm 月 dd 日までの日数を返す関数 int days(int mm, int dd).
 
-/* */
+/*
+  month_day[12]を作り、それぞれの月の日数を入れ、
+  for文でmm月の前までの日数をたし、最後にその数にdd日を足して、
+  1日スタートなので1を引いた値を返した。
+*/
+
+int days(int mm, int dd) {
+  int month_day[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+  int day = 0;
+  int i;
+
+  for (i = 0; i < mm - 1; i++) {
+    day += month_day[i];
+  }
+
+  return day + dd - 1;
+}
 
 // 42, 
 
@@ -22,6 +38,7 @@
 
 int main(void) {
   printf("No41\n");
+  printf("%i\n", days(2, 13));
 
   printf("No42\n");
 
