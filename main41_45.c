29d2c6ae816c1f336152b50c99bbec3614011a39
take-cantik <miki.takeru508@mail.kyutech.jp>
2021-01-04 21:46:20 +0900

    Do 42

--- main41_45.c
+++ main41_45.c
@@ -20,9 +20,51 @@ int days(int mm, int dd) {
   return day + dd - 1;
 }
 
-// 42, 
+// 42, y1 年 m1 月 d1 日から y2 年 m2 月 d2 月までの日数を返す関数 int days_between(int y1, int m1, int d1, int y2, int m2, int m2). 自分は今日まで何日生きてきましたか？
 
-/**/
+/*
+  まず、y1からy2までをそれぞれを含まず回して、閏年であれば366, そうでなければ365を足した。
+  次に、年が同じであればdays関数にそれぞれの月、日を引数としたものを引きそれをdayに足し、
+  そうでなければ、m1からd1からその年の最後までを足し、daysでy2のとしの最後までを合わせた。
+  最後にy1, y2が閏年の場合に1ずつ足す。
+*/
+
+int leap(int year) {
+  if (year % 400 == 0) {
+    return 1;
+  } else if (year % 100 == 0) {
+    return 0;
+  } else if (year % 4 == 0) {
+    return 1;
+  } else {
+    return 0;
+  }
+}
+
+int days_between(int y1, int m1, int d1, int y2, int m2, int d2) {
+  int day = 0;
+  int i;
+
+  for (i = y1 + 1; i < y2; i++) {
+    day += 365 + leap(i);
+  }
+
+  if (y1 == y2) {
+    day += days(m2, d2) - days(m1, d1);
+  } else {
+    day += 364 - days(m1, d1) + days(m2, d2);
+
+    if (leap(y1) && m1 < 3) {
+      day++;
+    }
+
+    if (leap(y2) && m2 > 2) {
+      day++;
+    }
+  }
+
+  return day;
+}
 
 // 43, 
 
@@ -38,9 +80,10 @@ int days(int mm, int dd) {
 
 int main(void) {
   printf("No41\n");
-  printf("%i\n", days(2, 13));
+  printf("%i\n", days(12, 31));
 
   printf("No42\n");
+  printf("%i\n", days_between(2000, 4, 17, 2021, 1, 4));
 
   printf("No43\n");
 
