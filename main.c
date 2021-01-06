71773c153b9b2edda4719e03d518f092fe6e00e8
take-cantik <miki.takeru508@mail.kyutech.jp>
2021-01-07 01:00:33 +0900

    Do 43

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+char *ahiahi(void) {
+  char *ahi;
+  ahi = "manji";
+  return ahi;
+}
+
+int main(void) {
+  printf("%s\n", ahiahi());
+
+  return 0;
+}
--- main41_45.c
+++ main41_45.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 // 41, 平年の1 月 1 日から同年 mm 月 dd 日までの日数を返す関数 int days(int mm, int dd).
 
@@ -66,9 +68,77 @@ int days_between(int y1, int m1, int d1, int y2, int m2, int d2) {
   return day;
 }
 
-// 43, 
+// 43, 時刻 h1:m1:s1 と 時刻 h2:m2:s2 をそれぞれ文字列として受け取り、 それらの時間差を文字列で戻す関数 char * times_between_string(char time1[ ], char time2[ ]).
 
-/**/
+/*
+  time_atoi関数は"hh:mm:ss"の形の文字列を:区切りでint型の配列に入れていく関数。
+  time_diff関数はその配列から差を求める関数。
+  link_str関数はtime_diffで作られた配列を文字列に戻す関数。
+  それを順に組み合わせた。
+  ただし、呼び出す際に文字列リテラルを与えると上手く動作しない。
+  御教授願います。
+*/
+
+void time_atoi(char time[], int time_int[]) {
+  char *time_str;
+
+  time_str = strtok(time, ":");
+  time_int[0] = atoi(time_str);
+
+  int i;
+  for (i = 1; time_str != NULL; i++) {
+    time_str = strtok(NULL, ":");
+
+    if (time_str != NULL) {
+      time_int[i] = atoi(time_str);
+    }
+  }
+}
+
+void time_diff(int ret_time[], int time1[], int time2[]) {
+  int down = 0;
+  int i;
+
+  for (i = 2; i >= 0; i--) {
+    if (time1[i] - time2[i] - down >= 0) {
+      ret_time[i] = time1[i] - time2[i] - down;
+      down = 0;
+    } else {
+      ret_time[i] = 0;
+      down = 1;
+    }
+  }
+}
+
+void link_str(char time[], int time_int[]) {
+  char time_str[3];
+  int i;
+
+  for (i = 0; i < 3; i++) {
+    sprintf(time_str, "%i", time_int[i]);
+    strcat(time, time_str);
+    if (i != 2) {
+      strcat(time, ":");
+    }
+  }
+}
+
+char *times_between_string(char time1[], char time2[]) {
+  int time_i1[3];
+  int time_i2[3];
+  time_atoi(time1, time_i1);
+  time_atoi(time2, time_i2);
+
+  int diff_time[3];
+  time_diff(diff_time, time_i1, time_i2);
+
+  char link_time[9];
+  link_str(link_time, diff_time);
+
+  char *ret_time = link_time;
+
+  return ret_time;
+}
 
 // 44, 3 桁の整数の桁を入れ替えた整数を返す関数 int rev3(int n). 例えば rev(314) の戻り値は 413 になる。310 は 13 だな。
 
@@ -107,6 +177,9 @@ int main(void) {
   printf("%i\n", days_between(2000, 4, 17, 2021, 1, 4));
 
   printf("No43\n");
+  char time1[] = "12:12:12";
+  char time2[] = "11:11:11";
+  printf("%s\n", times_between_string(time1, time2));
 
   printf("No44\n");
   printf("%i\n", rev3(150));
