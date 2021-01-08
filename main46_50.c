324a2688996e7bb7c9b0621772a45a146adb3b57
take-cantik <miki.takeru508@mail.kyutech.jp>
2021-01-08 20:53:01 +0900

    Do 46

--- main46_50.c
+++ main46_50.c
@@ -1,8 +1,19 @@
 #include <stdio.h>
 
-// 46, 
+// 46, 1~20 の各整数についてその2乗をプリントする関数 void squares_p(void).
 
-/**/
+// for文をiを1~20で回し、square関数にそれぞれ入れ、出力した。
+
+int square(int n) {
+  return n * n;
+}
+
+void squares_p(void) {
+  int i;
+    for (i = 1; i <= 20; i++) {
+      printf("%i\n", square(i));
+    }
+}
 
 // 47, 
 
@@ -22,6 +33,7 @@
 
 int main(void) {
   printf("No46\n");
+  squares_p();
 
   printf("No47\n");
 
