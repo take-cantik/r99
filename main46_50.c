c120410f8990cd070a2fcda9400e9ee8131d70dc
take-cantik <miki.takeru508@mail.kyutech.jp>
2021-01-08 20:43:01 +0900

    Create template of 46 to 50

---
+++ main46_50.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+// 46, 
+
+/**/
+
+// 47, 
+
+/**/
+
+// 48, 
+
+/**/
+
+// 49, 
+
+/**/
+
+// 50, 
+
+/**/
+
+int main(void) {
+  printf("No46\n");
+
+  printf("No47\n");
+
+  printf("No48\n");
+
+  printf("No49\n");
+
+  printf("No50\n");
+  
+  return 0;
+}
+
