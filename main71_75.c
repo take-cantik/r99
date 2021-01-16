be81406c3a20f5154ed6e2cff70b0d19a886078b
take-cantik <miki.takeru508@mail.kyutech.jp>
2021-01-16 23:21:27 +0900

    Create 71 to 75

---
+++ main71_75.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+// 71, 
+
+/**/
+
+// 72, 
+
+/**/
+
+// 73, 
+
+/**/
+
+// 74, 
+
+/**/
+
+// 75, 
+
+/**/
+
+int main(void) {
+  printf("No71\n");
+
+  printf("No72\n");
+
+  printf("No73\n");
+
+  printf("No74\n");
+
+  printf("No75\n");
+  
+  return 0;
+}
+
