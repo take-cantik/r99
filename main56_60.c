224a8b6d28e0f947b59aa022ec18f7af4cd7edbf
take-cantik <miki.takeru508@mail.kyutech.jp>
2021-01-25 13:37:17 +0900

    Do 56 and 57

--- main56_60.c
+++ main56_60.c
@@ -2,11 +2,36 @@
 
 // 56, ファイル integers.txt をダウンロードし、適当な場所にセーブしてください。以下、61番まではこのファイルを使います。 ファイル "integers.txt" には一行にひとつ、 整数が書き込まれている。 そのファイルの1行目の数を返す関数 int head(void).
 
-/**/
+/*fopenでファイルをreadモードでオープンし、fscanfで一つ整数を読み込んで最後にfcloseをしてとじた。*/
 
-// 57, 
+int head(void) {
+  FILE *fp;
+  int number;
 
-/**/
+  fp = fopen("integers.txt", "r");
+  fscanf(fp, "%i", &number);
+  fclose(fp);
+
+  return number;
+}
+
+// 57, ファイル "integers.txt" が何行あるかを返す関数 int lines(void).
+
+// fgetを用いて、その返り値がNULLになるまで変数lineに足していき、lineを返した。
+
+int lines(void) {
+  FILE *fp;
+  char line_num[100];
+  int line = 0;
+
+  fp = fopen("integers.txt", "r");
+  for (;fgets(line_num, 100, fp) != NULL;) {
+    line++;
+  }
+  fclose(fp);
+  
+  return line;
+}
 
 // 58, 
 
@@ -22,8 +47,10 @@
 
 int main(void) {
   printf("No56\n");
+  printf("%i\n", head());
 
   printf("No57\n");
+  printf("%i\n", lines());
 
   printf("No58\n");
 
