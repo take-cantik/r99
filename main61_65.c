b461eeeefe7c9d59df86004ffd0148dee7830da9
take-cantik <miki.takeru508@mail.kyutech.jp>
2021-01-25 17:57:06 +0900

    Create main 61 to 65

---
+++ main61_65.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// 61, 
+
+/**/
+
+// 62, 
+
+/**/
+
+// 63, 
+
+/**/
+
+// 64, 
+
+/**/
+
+// 65, 
+
+/**/
+
+int main(void) {
+  printf("No61\n");
+
+  printf("No62\n");
+
+  printf("No63\n");
+
+  printf("No64\n");
+
+  printf("No65\n");
+  
+  return 0;
+}
+
