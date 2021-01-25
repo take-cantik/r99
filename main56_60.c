3a6edac2d9871dce64afa8f42e82f384a9dddce0
take-cantik <miki.takeru508@mail.kyutech.jp>
2021-01-25 13:03:43 +0900

    Create 56 to 60

---
+++ main56_60.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+// 56, ファイル integers.txt をダウンロードし、適当な場所にセーブしてください。以下、61番まではこのファイルを使います。 ファイル "integers.txt" には一行にひとつ、 整数が書き込まれている。 そのファイルの1行目の数を返す関数 int head(void).
+
+/**/
+
+// 57, 
+
+/**/
+
+// 58, 
+
+/**/
+
+// 59, 
+
+/**/
+
+// 60, 
+
+/**/
+
+int main(void) {
+  printf("No56\n");
+
+  printf("No57\n");
+
+  printf("No58\n");
+
+  printf("No59\n");
+
+  printf("No60\n");
+  
+  return 0;
+}
+
