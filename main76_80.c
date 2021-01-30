da088e44891fef1db46031f5d971870db1ff7bf0
take-cantik <miki.takeru508@mail.kyutech.jp>
2021-01-31 06:14:55 +0900

    Create main 76 to 80

---
+++ main76_80.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+// 76, 
+
+/**/
+
+// 77, 
+
+/**/
+
+// 78, 
+
+/**/
+
+// 79, 
+
+/**/
+
+// 80, 
+
+/**/
+
+int main(void) {
+  printf("No76\n");
+
+  printf("No77\n");
+
+  printf("No78\n");
+
+  printf("No79\n");
+
+  printf("No80\n");
+  
+  return 0;
+}
+
