d857f1ff76dcaa5086f34e3fb2990caf0b37921a
take-cantik <miki.takeru508@mail.kyutech.jp>
2021-01-31 19:05:09 +0900

    Do 82

--- main81_85.c
+++ main81_85.c
@@ -32,9 +32,27 @@ int str_eql(char *s1, char *s2) {
   return 1;
 }
 
-// 82, 
+// 82, 二つの文字列 s1, s2 の先頭の n 文字が等しいかどうかを判定する関数 int str_eql_n(char* s1, char* s2, int n).
 
-/**/
+/*
+  for文でiをn回回して、まずs1, s2のi番目の要素が'\0'だったら0を返して、
+  次に、s1, s2のi番目の要素が違ったら0を返す。
+  最後にfor文を抜けたら1を返した。
+*/
+
+int str_eql_n(char *s1, char *s2, int n) {
+  int i;
+
+  for (i = 0; i < n; i++) {
+    if (s1[i] == '\0' || s2[i] == '\0') {
+      return 0;
+    }
+    if (s1[i] != s2[i]) {
+      return 0;
+    }
+  }
+  return 1;
+}
 
 // 83, 
 
@@ -50,11 +68,15 @@ int str_eql(char *s1, char *s2) {
 
 int main(void) {
   printf("No81\n");
-  char str1[] = "ahiahi";
-  char str2[] = "ahiahi";
-  printf("%i\n", str_eql(str1, str2));
+  char s01[] = "ahiahi";
+  char s02[] = "ahiahi";
+  printf("%i\n", str_eql(s01, s02));
 
   printf("No82\n");
+  char s11[] = "qwertyuiop";
+  char s12[] = "qwertasdfg";
+  printf("%i\n", str_eql_n(s11, s12, 6));
+  printf("%i\n", str_eql_n(s11, s12, 5));
 
   printf("No83\n");
 
