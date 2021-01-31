17bfa279e355d210405d3f95bebf8cda24f64039
take-cantik <miki.takeru508@mail.kyutech.jp>
2021-01-31 17:53:31 +0900

    Create 81 to 85

---
+++ main81_85.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+// 81, 
+
+/**/
+
+// 82, 
+
+/**/
+
+// 83, 
+
+/**/
+
+// 84, 
+
+/**/
+
+// 85, 
+
+/**/
+
+int main(void) {
+  printf("No81\n");
+
+  printf("No82\n");
+
+  printf("No83\n");
+
+  printf("No84\n");
+
+  printf("No85\n");
+  
+  return 0;
+}
+
