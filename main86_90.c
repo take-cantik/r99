245d93e9308f73126686e7a97e96a74c155a9147
take-cantik <miki.takeru508@mail.kyutech.jp>
2021-01-31 21:22:53 +0900

    Create 86 to 90

---
+++ main86_90.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+// 86, 
+
+/**/
+
+// 87, 
+
+/**/
+
+// 88, 
+
+/**/
+
+// 89, 
+
+/**/
+
+// 90, 
+
+/**/
+
+int main(void) {
+  printf("No86\n");
+
+  printf("No87\n");
+
+  printf("No88\n");
+
+  printf("No89\n");
+
+  printf("No90\n");
+  
+  return 0;
+}
+
